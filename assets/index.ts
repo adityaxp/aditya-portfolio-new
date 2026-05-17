99af3f14a39869e9a0db08ab43bf62482940bf6a
Aditya <aditya@one800.help>
2026-05-17 14:42:15 +0530

    chore: add CDN assets

---
+++ assets/index.ts
@@ -0,0 +1,27 @@
+const p2pChat =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fp2p-chat%2Fp2p-chat.png?alt=media&token=0e6dac14-216a-48c4-bd1f-5b842a3e9dd1";
+const p2pChatHover =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fp2p-chat%2Fp2p-chat-hover.gif?alt=media&token=9d902f1d-261e-46e5-98b4-dcb61c20c147";
+const lawSage =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Flawsage%2Flawsage.png?alt=media&token=58e40bf4-9ddd-4dd0-baa4-c4d87e38ad67";
+const lawSageHover =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Flawsage%2Flawsage-hover.gif?alt=media&token=f6659d51-76af-4792-88da-0e7d38ec5d09";
+const raspiCarV2 =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fraspi-car-v2%2Fraspi-car-v2.jpg?alt=media&token=c17c9210-589f-4b8d-9374-468819023d9a";
+const raspiCarV2Hover =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fraspi-car-v2%2Fraspi-car-v2-hover.gif?alt=media&token=a4269c3a-eeb4-4886-939c-d82eb7d01b2c";
+const ravenUAV =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fraven-uav%2Fraven-uav.jpeg?alt=media&token=a7b08911-2f59-4fc8-9191-9ee99c7d81da";
+const ravenUAVHover =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fraven-uav%2Fraven-uav-hover.gif?alt=media&token=83cab41c-032a-4df7-80a1-8aa05adc87a8";
+
+export {
+  p2pChat,
+  p2pChatHover,
+  lawSage,
+  lawSageHover,
+  raspiCarV2,
+  raspiCarV2Hover,
+  ravenUAV,
+  ravenUAVHover,
+};
