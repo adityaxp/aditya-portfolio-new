3affbf757acb0151dc44b7808a4df3b792925b3d
Aditya <aditya@one800.help>
2026-05-19 00:25:50 +0530

    chore: update robots.txt to disallow access to portfolio-analytics

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: "/portfolio-analytics",
     },
     sitemap: `${siteConfig.url}/sitemap.xml`,
   };
