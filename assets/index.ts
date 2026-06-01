52301e64903779d555271d4935fba4e04d78e7c5
Aditya Balsane <61054881+adityaxp@users.noreply.github.com>
2026-06-01 11:28:58 +0530

    chore: update assets

--- assets/index.ts
+++ assets/index.ts
@@ -10,11 +10,8 @@ const raspiCarV2 =
   "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fraspi-car-v2%2Fraspi-car-v2.jpg?alt=media&token=c17c9210-589f-4b8d-9374-468819023d9a";
 const raspiCarV2Hover =
   "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fraspi-car-v2%2Fraspi-car-v2-hover.gif?alt=media&token=a4269c3a-eeb4-4886-939c-d82eb7d01b2c";
-const ravenUAV =
-  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fraven-uav%2Fraven-uav.jpeg?alt=media&token=a7b08911-2f59-4fc8-9191-9ee99c7d81da";
-const ravenUAVHover =
-  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fraven-uav%2Fraven-uav-hover.gif?alt=media&token=83cab41c-032a-4df7-80a1-8aa05adc87a8";
-
+const ravenUAV = "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fraven-uav%2Fraven.png?alt=media&token=b7df9a94-e61d-4c4c-99f4-33f46b9e437d";
+const ravenUAVHover = "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-ba601.appspot.com/o/new-portfolio%2Fprojects%2Fraven-uav%2Fraven-uav-hover.gif?alt=media&token=93cded9f-57d1-4c3a-925c-ac719602208b";
 export {
   p2pChat,
   p2pChatHover,
